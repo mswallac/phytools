FIX/ADDITION : better handling of false negative calculation, check initial value is zero / added phy splitting action for automated splitter process.

outlier_bench/outlier.py
@@ -49,12 +49,14 @@ def run_exp_outlier(exp_clust,s,m,c):
         return None,None,None,None
 
 def bench_outlier(mstdict,outs,outs_each_iter,real_spks,hyb_spks,spikes):
+    n_art = len(hyb_spks)
     # Calculate performance metrics on orig cluster
     TP = sum(np.in1d(spikes,hyb_spks))
     FP = sum(np.in1d(spikes,real_spks))
-    FN = len(hyb_spks)-TP
-    assert (TP+FP) == len(spikes)
+    FN = n_art-TP
     before_f1 = TP/(TP+(0.5*(FP+FN)))
+    assert (TP+FP) == len(spikes)
+    assert FN == 0 
 
     f1_onstep = []
     prec_rem_onstep = []
@@ -86,7 +88,7 @@ def bench_outlier(mstdict,outs,outs_each_iter,real_spks,hyb_spks,spikes):
         # Calculate performance metrics
         TP = sum(np.in1d(hyb_spks,remaining_spks))
         FP = sum(np.in1d(real_spks,remaining_spks))
-        FN = len(hyb_spks)-TP
+        FN = n_art-TP
         F1 = TP/(TP+(0.5*(FP+FN)))
         assert (TP+FP) == len(remaining_spks)
         f1_onstep.append(F1)

splitter_bench/split.py
@@ -15,7 +15,13 @@ def run_exp_split(exp_clust,s,m,c,nclusts):
         cid,spikes,nspikes,chan,mstdict = get_spikes([cid],m,c)
         splits = cluster(mstdict,spikes,list(mstdict.keys())[2:8],nclusts)
         clust_precisions,f1s_merged,merged_clusts,f1_ba = merge_clusters(splits,real_spks,hyb_spks,spikes,nclusts)
-        #s.actions.split(merged['r'])
+
+        sts = []
+        for x in merged_clusts[np.argmax(f1s_merged)]:
+            sts.extend(splits[x])
+        
+        s.actions.split(sts)
+        
         return art_pct,clust_precisions,f1s_merged,merged_clusts,f1_ba
     else:
         return None,None,None,None,None
@@ -72,11 +78,14 @@ def merge_clusters(splits,real_spikes,hyb_spikes,spikes,nclusts):
     # Figure out what % each cluster is composed of each given cluster
     keys = list(splits.keys())
     clust_precisions = []
+    n_art = len(hyb_spikes)
 
     TP = sum(np.in1d(hyb_spikes,spikes))
     FP = sum(np.in1d(real_spikes,spikes))
+    FN = n_art-TP
     assert (TP+FP) == len(spikes)
-    FN = len(hyb_spikes)-TP
+    assert FN == 0 
+
     before_F1 = TP/(TP+0.5*(FP+FN))
 
     # Using precision as metric to decide merge order
@@ -85,7 +94,7 @@ def merge_clusters(splits,real_spikes,hyb_spikes,spikes,nclusts):
         TP = sum(np.in1d(hyb_spikes,split_spikes))
         FP = sum(np.in1d(real_spikes,split_spikes))
         assert (TP+FP) == len(split_spikes)
-        FN = len(hyb_spikes)-TP
+        FN = n_art-TP
         clust_precisions.append(TP/(TP+FP))
 
     clust_precisions=np.array(clust_precisions)
@@ -101,7 +110,7 @@ def merge_clusters(splits,real_spikes,hyb_spikes,spikes,nclusts):
         TP = sum(np.in1d(hyb_spikes,merge_spikes))
         FP = sum(np.in1d(real_spikes,merge_spikes))
         assert (TP+FP) == len(merge_spikes)
-        FN = len(hyb_spikes)-TP
+        FN = n_art-TP
         F1 = TP/(TP+0.5*(FP+FN))
         f1s_merged.append(F1)
         merged_clusts.append(sort_map[0:i])